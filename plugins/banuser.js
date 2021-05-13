44f606420e7aeedb899ec85ea319d32602466427
919544707896DEVIL <84152912+919544707896DEVIL@users.noreply.github.com>
2021-05-14 01:20:47 +0530

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Siapa yang mau di banned?'
+    if (!text) throw '*Tag anyone & use this command*'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw '*Tag One*'
     let users = global.DATABASE._data.users
     users[who].banned = true
-    conn.reply(m.chat, `berhasil banned`, m)
+    conn.reply(m.chat, `*User Banned SucessFully!! ✅️*`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
