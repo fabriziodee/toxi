a9905b11d0bdb7321084871a2b7d833d77254363
919544707896DEVIL <84152912+919544707896DEVIL@users.noreply.github.com>
2021-05-14 11:34:06 +0530

    Update enhance.js

--- plugins/enhance.js
+++ plugins/enhance.js
@@ -4,8 +4,8 @@ const FormData = require('form-data')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  if (!mime) throw '*Give me a photo*'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
   let img = await q.download()
   let body = new FormData
   body.append('image', img, 'image')
