a0f85ac59d3f1dc1b91afbf26556c08a20ce5d4a
919544707896DEVIL <84152912+919544707896DEVIL@users.noreply.github.com>
2021-05-14 17:31:19 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -85,7 +85,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     let before = conn.menu.before || `
 *「 All Existing Commands 」*
 %readmore`
-    let header = conn.menu.header || '╓─── *【 %category 】*
+    let header = conn.menu.header || '╓─── *【 %category 】*'
     let body   = conn.menu.body   || '║➥ *%cmd%islimit*'
     let footer = conn.menu.footer || '╙───────々\n'
     let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
