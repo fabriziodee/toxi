c48a67e0e1ba1bd6f4c23e2df87a0fdb6cf30748
919544707896DEVIL <84152912+919544707896DEVIL@users.noreply.github.com>
2021-05-14 18:10:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('A SIMPLE WHATSAPP BOT\nMADE BY TOXIC DEVIL', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+CFonts.say(`'WhatsApp Bot By TOXIC DEVIL'`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
