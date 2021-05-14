a674505ad971d3112d7f347600b2941640945742
919544707896DEVIL <84152912+919544707896DEVIL@users.noreply.github.com>
2021-05-14 18:49:00 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -3,27 +3,15 @@ let fs = require('fs')
 let path = require('path')
 let { spawn } = require('child_process')
 let handler = async (m, { conn, args }) => {
-  let lang = 'id'
-  let text = args.slice(1).join(' ')
-  if (args[0].length === 2) lang = args[0]
-  else text = args.join(' ')
-  if (!text) text = lang
-  let res
-  try { res = await tts(text, lang) }
-  catch (e) {
-    m.reply(e + '')
-    res = await tts(text)
-  } finally {
-    conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
-  }
+  if (args.length < 2) return m.reply('*Must Enter Some Text*')
+  conn.sendFile(m.chat, await tts(args[0], args.slice(1).join(' ')).catch(err => m.reply(err + '')), 'tts.opus', null, m, true)
 }
-handler.help = ['tts <lang> <teks>']
+handler.help = ['tts']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
 module.exports = handler
 
-function tts(text, lang = 'id') {
-  console.log(lang, text)
+function tts(lang, text) {
   return new Promise((resolve, reject) => {
     try {
       let tts = gtts(lang)
